6e81a524216c5e612e11155088da07c59f49a34e
saintlaur <117801567+saintlaur@users.noreply.github.com>
2023-03-31 23:28:23 +0300

    Update main.lua

--- client/main.lua
+++ client/main.lua
@@ -253,12 +253,14 @@ local Functions = {
     end,
     CloneTable = function(self, table)
         local copy = {}
-        for k,v in pairs(table) do
-            if type(v) == 'table' then
-                copy[k] = self:CloneTable(v)
-            else
-                if type(v) == 'function' then v = nil end
-                copy[k] = v
+        if table then
+            for k,v in pairs(table) do
+                if type(v) == 'table' then
+                    copy[k] = self:CloneTable(v)
+                else
+                    if type(v) == 'function' then v = nil end
+                    copy[k] = v
+                end
             end
         end
         return copy
